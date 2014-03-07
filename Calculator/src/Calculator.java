cff2c445102b4d95af2207309376b457e3f5e1b6
RocketJ <richard.seidel@mercy.net>
2014-03-07 12:28:43 -0600

    Test

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -2,7 +2,7 @@
 public class Calculator {
 	public long add (long a, long b) {
 		return a + b;
-	} // end Method add
+	} // end Method add --
 	
 	public long subtract (long a, long b) {
 		return a - b;
