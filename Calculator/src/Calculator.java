e26b898627a38f4e68cd34b66f80aa96ad05be2a
RocketJ <richard.seidel@mercy.net>
2014-03-07 10:31:09 -0600

    Testing

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,18 @@
+
+public class Calculator {
+	public long add (long a, long b) {
+		return a + b;
+	}
+	
+	public long subtract (long a, long b) {
+		return a - b;
+	}
+	
+	public long multiply (long a, long b) {
+		return a * b;
+	}
+	
+	public long divide (long a, long b) {
+		return a / b;
+	}
+}
