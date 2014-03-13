85da3e227bb0ab2b40992b28b4995ef985eab15c
RocketJ <richard.seidel@mercy.net>
2014-03-13 11:50:28 -0500

    Maven test

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+
+public class Calculator {
+	public long add (long a, long b) {
+		return a + b;
+	} // end Method add --
+	
+	public long subtract (long a, long b) {
+		return a - b;
+	} // end Method subtract
+	
+	public long multiply (long a, long b) {
+		return a * b;
+	} // end Method multiply
+	
+	public long divide (long a, long b) {
+		return a / b;
+	} // end Method divide
+}
